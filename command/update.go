eb7590ee71f28d38f2c83bc670ca984796c54b77
n/a <n/a>
2022-06-07 14:23:14 -0400

    replies to feature added

--- command/update.go
+++ command/update.go
@@ -17,7 +17,7 @@ type Post struct {
     Filename string
     Fileinfo string
     Imgprev string
-    Replies []string
+    Replies []int
 }
 
 type Thread struct {
@@ -32,11 +32,17 @@ func get_posts(parent int) []*Post {
 	Err_check(err)
 
 	stmt, err := conn.Prepare(`SELECT Id, Content, Time, COALESCE(File, '') AS File, COALESCE(Filename, '') AS Filename, 
-		COALESCE(Fileinfo, '') AS Fileinfo, COALESCE(Imgprev, '') Imgprev FROM posts WHERE parent = ?`)
+		COALESCE(Fileinfo, '') AS Fileinfo, COALESCE(Imgprev, '') Imgprev FROM posts WHERE Parent = ?`)
 	Err_check(err)
 
 	//make another statment that searches the replies table(every board will have one) 
 	//where the source equals the given id, add the replier to the post's replies array 
+	
+	conn2, err := sql.Open("sqlite3", BP + "command/post-coll.db")
+	Err_check(err)
+
+	stmt2, err := conn2.Prepare(`Select Replier FROM replies WHERE Source = ?`)
+	Err_check(err)
 
 	rows, err := stmt.Query(parent)
 	Err_check(err)
@@ -49,6 +55,17 @@ func get_posts(parent int) []*Post {
 		err = rows.Scan(&pst.Id, &pst.Content, &pst.Time, &pst.File,
 			&pst.Filename, &pst.Fileinfo, &pst.Imgprev)
 		Err_check(err)
+
+		rep_rows, err := stmt2.Query(pst.Id)
+		Err_check(err)
+
+		for rep_rows.Next() {
+			var replier int
+			rep_rows.Scan(&replier)
+			pst.Replies = append(pst.Replies, replier)
+		}
+		
+		rep_rows.Close()
 		thread_body = append(thread_body, &pst)
 	}
 
