a9defd89e14213d387a60a42b9f6759e91bc5bcb
n/a <n/a>
2022-06-12 23:47:46 -0400

    error logging added

--- command/ogai.go
+++ command/ogai.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"flag"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -33,6 +34,10 @@ func main() {
 
 	flag.StringVar(&BP, "BP", "/mnt/c/server/data/content/media/toggle/", "the base path")
 	flag.Parse()
+
+	file, err := os.OpenFile("error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	Err_check(err)
+	log.SetOutput(file)
 
 	//New_db() 
 	Make_Conns()
