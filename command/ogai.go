9a22166afdb196da91be387d90c8eeed04864b39
anon <n/a>
2022-10-28 11:13:00 -0400

    re-added ogai.go

---
+++ command/ogai.go
@@ -0,0 +1,69 @@
+package main
+
+
+import (
+
+	"database/sql"
+	"log"
+	"os"
+	"math/rand"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+
+const rand_charset = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+
+
+func Err_check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func Query_err_check(err error) {
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// there were no rows, but otherwise no error occurred
+
+		} else {
+				log.Fatal(err)
+			}
+	}
+}
+
+
+func Rand_gen() string {
+
+    result := ""
+
+    for i := 0; i < 6; i++ {
+
+        c := rand_charset[rand.Intn(len(rand_charset))]
+
+	result += string(c)
+    }
+
+    return result
+}
+
+
+func main() {
+
+	Load_conf()
+
+	file, err := os.OpenFile(BP + "error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	Err_check(err)
+	log.SetOutput(file)
+
+	rand.Seed(time.Now().UnixNano())
+
+	//New_db()
+	Make_Conns()
+	//Build_thread() 
+	Listen()
+
+}
