a4bcad40bcf54989d1ea297713035798511097b4
anon <n/a>
2022-11-15 21:01:11 -0500

    home tables created

--- command/newdb.go
+++ command/newdb.go
@@ -44,6 +44,21 @@ func create_table(db *sql.DB) {
         "Id" INTEGER NOT NULL
     );`
 
+    createHomePostTableSQL := `CREATE TABLE homepost (
+        "Board" TEXT NOT NULL,
+        "Id" INTEGER NOT NULL,
+        "Content" TEXT NOT NULL,
+        "Parent" INTEGER NOT NULL
+    );`
+
+    createHomeThumbTableSQL := `CREATE TABLE homethumb (
+        "Board" TEXT NOT NULL,
+        "Id" INTEGER NOT NULL,
+        "Parent" TEXT NOT NULL,
+        "Imgprev" TEXT NOT NULL
+    );`
+
+    //triggers
     createLatestTriggerSQL := `CREATE TRIGGER latest_update
         AFTER INSERT ON posts
         BEGIN
@@ -52,6 +67,23 @@ func create_table(db *sql.DB) {
             WHERE Board = NEW.Board;
         END;`
 
+    trimHomePostStack := `CREATE TRIGGER homepost_trim
+        AFTER INSERT ON homeposts
+        BEGIN
+            DELETE FROM homeposts WHERE ROWID =
+                IIF((SELECT COUNT(Id) FROM homeposts) > 15,
+                (SELECT min(ROWID) from homeposts), NULL);
+        END;`
+
+    trimHomeThumbStack := `CREATE TRIGGER homethumb_trim
+        AFTER INSERT ON homethumb
+        BEGIN
+            DELETE FROM homethumb WHERE ROWID =
+                IIF((SELECT COUNT(Id) FROM homethumb) > 5,
+                (SELECT min(ROWID) from homethumb), NULL);
+        END;`
+        
+    //how new posts know what their id is 
     latestseedSQL := `INSERT INTO latest (Board, Id) VALUES (cb, 1);`
 
 
@@ -71,10 +103,25 @@ func create_table(db *sql.DB) {
         Err_check(err)
     statement.Exec()
 
+    statement, err = db.Prepare(createHomePostTableSQL)
+        Err_check(err)
+    statement.Exec()
+
+    statement, err = db.Prepare(createHomeThumbTableSQL)
+        Err_check(err)
+    statement.Exec()
+
     statement, err = db.Prepare(createLatestTriggerSQL)
         Err_check(err)
     statement.Exec()
 
+    statement, err = db.Prepare(trimHomePostStack)
+        Err_check(err)
+    statement.Exec()
+
+    statement, err = db.Prepare(trimHomeThumbStack)
+        Err_check(err)
+    statement.Exec()
 
     for _, board := range Boards {
         statement, err = db.Prepare(strings.Replace(latestseedSQL, "cb", `'` + board + `'`, 1))
